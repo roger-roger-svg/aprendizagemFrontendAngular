ca91f125e3b5c0cda045c36a681813d6ab4b5646
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-11-05 23:44:02 -0300

    feito o componente adicionar atividades

---
+++ src/app/components/UnidadeCurricular/adicionar-atividades/adicionar-atividades.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-atividades',
+  templateUrl: './adicionar-atividades.component.html',
+  styleUrls: ['./adicionar-atividades.component.css'],
+})
+export class AdicionarAtividadesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
