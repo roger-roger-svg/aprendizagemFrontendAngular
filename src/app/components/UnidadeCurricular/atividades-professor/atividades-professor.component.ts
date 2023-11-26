e31af9bf58770fdff3d4c4068e64c441c6b38e24
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-11-26 17:31:43 -0300

    add pagina atividades professor

--- src/app/components/UnidadeCurricular/atividades-professor/atividades-professor.component.ts
+++ src/app/components/UnidadeCurricular/atividades-professor/atividades-professor.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { EditorModule } from 'primeng/editor';
+import { UsuarioProximasAtividadesComponent } from '../../Dashboard/usuario-dashboard/usuario-proximas-atividades/usuario-proximas-atividades.component';
 @Component({
   selector: 'app-atividades-professor',
   templateUrl: './atividades-professor.component.html',
   styleUrls: ['./atividades-professor.component.css'],
 })
 export class AtividadesProfessorComponent implements OnInit {
+  menuItems = [
+    { label: 'Histórico de Atividades', icon: 'pi pi-list' },
+    { label: 'Editar Atividades', icon: 'pi pi-pencil' },
+    { label: 'Agendar Atividades', icon: 'pi pi-calendar' },
+    { label: 'Conceitos', icon: 'pi pi-book' },
+    { label: 'Configurações', icon: 'pi pi-cog' },
+    { label: 'Arquivos Privados', icon: 'pi pi-lock' },
+    { label: 'Avaliações', icon: 'pi pi-star' },
+    { label: 'Calendário', icon: 'pi pi-calendar-plus' },
+  ];
   constructor() {}
   text: string | undefined;
   activityTitle: string = '';
